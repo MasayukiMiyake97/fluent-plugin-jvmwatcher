0c670e7e1e623fccff4750294f46c4f1101ab64f
Masayuki Miyake <miyake@hayate.(none)>
2013-05-06 23:15:33 +0900

    edit test code

--- test/plugin/test_in_jvmwatcher.rb
+++ test/plugin/test_in_jvmwatcher.rb
@@ -6,51 +6,18 @@ class JvmWatcherInputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    tag jvmwatcher.log
   ]
-  # CONFIG = %[
-  #   path #{TMP_DIR}/out_file_test
-  #   compress gz
-  #   utc
-  # ]
 
-  def create_driver(conf = CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::DataCounterOutput, tag).configure(conf)
+  def create_driver(conf = CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::ScribeInput).configure(conf)
   end
 
   def test_configure
-    #### set configurations
-    # d = create_driver %[
-    #   path test_path
-    #   compress gz
-    # ]
+    d = create_driver
     #### check configurations
-    # assert_equal 'test_path', d.instance.path
-    # assert_equal :gz, d.instance.compress
+    assert_equal 'jvmwatcher.log', d.instance.tag
   end
 
-  def test_format
-    d = create_driver
 
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":1}\n]
-    # d.expect_format %[2011-01-02T13:14:15Z\ttest\t{"a":2}\n]
-
-    # d.run
-  end
-
-  def test_write
-    d = create_driver
-
-    # time = Time.parse("2011-01-02 13:14:15 UTC").to_i
-    # d.emit({"a"=>1}, time)
-    # d.emit({"a"=>2}, time)
-
-    # ### FileOutput#write returns path
-    # path = d.run
-    # expect_path = "#{TMP_DIR}/out_file_test._0.log.gz"
-    # assert_equal expect_path, path
-  end
 end
