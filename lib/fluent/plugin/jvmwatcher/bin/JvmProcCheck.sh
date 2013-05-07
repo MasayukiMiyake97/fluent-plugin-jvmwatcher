0225dda479be8bd9d9a6e7ee09ebc6088939bdb6
MasayukiMiyake <masayukimiyake97@gmail.com>
2013-05-07 22:42:03 +0900

    change commang param & config pattern

--- lib/fluent/plugin/jvmwatcher/bin/JvmProcCheck.sh
+++ lib/fluent/plugin/jvmwatcher/bin/JvmProcCheck.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# get JDK path
-pathtojava=$(readlink -e $(which javac))
-JDK_LIB=${pathtojava%/*/*}/lib
+source setEnv.sh
 
-export CLASSPATH=$CLASSPATH:../lib/jvmwatcher.jar
-export CLASSPATH=$CLASSPATH:../lib/jackson-core-2.2.0.jar
-export CLASSPATH=$CLASSPATH:../lib/commons-logging-1.1.2.jar
-export CLASSPATH=$CLASSPATH:../lib/log4j-1.2.17.jar
+# get JDK path
+#pathtojava=$(readlink -e $(which javac))
+#JDK_LIB=${pathtojava%/*/*}/lib
+
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/jvmwatcher.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/jackson-core-2.2.0.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/commons-logging-1.1.2.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/log4j-1.2.17.jar
 export CLASSPATH=$CLASSPATH:$JDK_LIB/tools.jar
-export CLASSPATH=$CLASSPATH:../config
+export CLASSPATH=$CLASSPATH:$WATCHER_CONFIGPATH
+
+echo $1
 
 java org.fluentd.jvmwatcher.JvmWatcher targetlist $1
 
--- lib/fluent/plugin/jvmwatcher/bin/JvmWatcher.sh
+++ lib/fluent/plugin/jvmwatcher/bin/JvmWatcher.sh
@@ -3,16 +3,16 @@
 localhostname=$(hostname)
 
 # get JDK path
-pathtojava=$(readlink -e $(which javac))
-JDK_LIB=${pathtojava%/*/*}/lib
+#pathtojava=$(readlink -e $(which javac))
+#JDK_LIB=${pathtojava%/*/*}/lib
 
-export CLASSPATH=$CLASSPATH:../lib/jvmwatcher.jar
-export CLASSPATH=$CLASSPATH:../lib/jackson-core-2.2.0.jar
-export CLASSPATH=$CLASSPATH:../lib/commons-logging-1.1.2.jar
-export CLASSPATH=$CLASSPATH:../lib/log4j-1.2.17.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/jvmwatcher.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/jackson-core-2.2.0.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/commons-logging-1.1.2.jar
+export CLASSPATH=$CLASSPATH:$WATCHER_LIBPATH/log4j-1.2.17.jar
 export CLASSPATH=$CLASSPATH:$JDK_LIB/tools.jar
-export CLASSPATH=$CLASSPATH:../config
+export CLASSPATH=$CLASSPATH:$WATCHER_CONFIGPATH
 
-java org.fluentd.jvmwatcher.JvmWatcher watcher $1 $localhostname
+java org.fluentd.jvmwatcher.JvmWatcher watcher $1 $2 $3 $4 $localhostname
 
 exit 0
