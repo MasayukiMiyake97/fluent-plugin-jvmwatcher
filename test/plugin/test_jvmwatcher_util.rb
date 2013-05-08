c99f10e21dcaf609631d5cb3c7ab47e6edf295a8
MasayukiMiyake <masayukimiyake97@gmail.com>
2013-05-09 00:02:52 +0900

    edit test code

--- test/plugin/test_jvmwatcher_util.rb
+++ test/plugin/test_jvmwatcher_util.rb
@@ -6,12 +6,25 @@ class JvmWatcherUtilTest < Test::Unit::TestCase
     Fluent::Test.setup
   end
 
-  def test_find_java_bin_path
-    path = JvmwatcherUtil.find_java_bin_path
-  end
-
-  def test_find_java_config_path
-    path = JvmwatcherUtil.find_java_config_path
+  def test_find_watcher_java_path
+    path = JvmwatcherUtil.find_watcher_java_path("bin")
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("lib")
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("config")
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("log")
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("logg")
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("bin", JvmwatcherUtil::SetEnvFileName)
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("log", JvmwatcherUtil::Log4JFileName)
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("bin", JvmwatcherUtil::SetEnvTemplateFileName)
+    puts path
+    path = JvmwatcherUtil.find_watcher_java_path("log", JvmwatcherUtil::Log4JTemplateFileName)
+    puts path
   end
 
 end
